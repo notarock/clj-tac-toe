5bfc9f4468ce21a2662c0be379bc1203ad1a2505
Roch D'Amour <roch.damour@gmail.com>
2019-09-07 00:54:55 -0400

    Add core data structure & play-move function

--- src/cljiktaktoe/core.clj
+++ src/cljiktaktoe/core.clj
@@ -1,7 +1,22 @@
 (ns cljiktaktoe.core
   (:gen-class))
 
+(def new-grid [[1 0 0]
+               [2 0 0]
+               [3 0 0]])
+
+(defn play-move
+  "Play a player's move in the tic-tac-toe grid"
+  [grid player move]
+  (let [x (- (first move) 1)
+        y (- (second move) 1)]
+    (assoc grid y (assoc (grid y) x player))))
+
+(play-move new-grid 'x [3 3])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, tic-tac-toe!"))
+
+
